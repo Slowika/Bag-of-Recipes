f79d2f4a08cb015b1bf6e076394fde41fcc61bee
Paula <paula.fermin.cueto@gmail.com>
2018-04-02 10:53:38 +0100

    Error handling ingredients 0 count in similarity function, pca on recipes

--- similarity_functions.py
+++ similarity_functions.py
@@ -9,6 +9,12 @@ def jaccard(X):
         Inputs:
             X: dataframe of recipes x ingredients
     """
+    # Initialise count of pairs of ingredients that have zero recipes
+    nullCount = 0
+
+    # List to store ingredients with zero count
+    problematicIngr = []
+
     # Create the set of recipes that contain each ingredient
     recipe_set = [ np.array(X.index[X[ingr] != 0]) for ingr in X.columns]
 
@@ -21,7 +27,25 @@ def jaccard(X):
             if ij > ii:
                 intersect = len(set.intersection(*[set(recipe_set[ii]), set(recipe_set[ij])]))
                 union     = len(set.union(*[set(recipe_set[ii]), set(recipe_set[ij])]))
+
+                if len(recipe_set[ii]) == 0 or len(recipe_set[ij])==0: pass
+
+                # Pairs of ingredients with empty union
+                if len(recipe_set[ii]) == 0 and ingri not in problematicIngr:
+                    problematicIngr.append(ingri)
+                    nullCount += 1
+                if len(recipe_set[ij]) == 0 and ingrj not in problematicIngr:
+                    problematicIngr.append(ingrj)
+                    nullCount += 1
+
                 jacs[ii, ij] = jacs[ij, ii] = intersect/float(union)
+
+    if nullCount > 0:
+        print("{:d} ingredients with zero frequency happened, similarities involving this ingredient = 0".format(nullCount))
+        print(problematicIngr)
+
+    # Empty diagonal
+    np.fill_diagonal(jacs, 0)
 
     return jacs
 
@@ -40,13 +64,34 @@ def asymmetric_cosine(X, alpha = 0.2):
     # Initialise matrix of asymmetric similarity
     asims = np.zeros((X.shape[1], X.shape[1]))
 
+    # Initialise count of pairs of ingredients that have zero recipes
+    nullCount = 0
+
+    # List to store ingredients with zero count
+    problematicIngr = []
+
     # Compute similarity for each pair of ingredients
     for ii, ingri in enumerate(X.columns):
         for ij, ingrj in enumerate(X.columns):
             intersect = len(set.intersection(*[set(recipe_set[ii]), set(recipe_set[ij])]))
-            union     = len(set.union(*[set(recipe_set[ii]), set(recipe_set[ij])]))
-            asims[ii, ij] = intersect/ ( len(recipe_set[ii])**alpha * len(recipe_set[ij])**(1-alpha) )
+            denom     = len(recipe_set[ii])**alpha * len(recipe_set[ij])**(1-alpha)
 
+            # Pairs of ingredients with empty union
+            if denom == 0:
+                if len(recipe_set[ii]) == 0 and ingri not in problematicIngr:
+                    problematicIngr.append(ingri)
+                    nullCount += 1
+                if len(recipe_set[ij]) == 0 and ingrj not in problematicIngr:
+                    problematicIngr.append(ingrj)
+                    nullCount += 1
+            else:
+                asims[ii, ij] = intersect/ denom
+
+    if nullCount > 0:
+        print("{:d} ingredients with zero frequency happened, similarity 0 returned".format(nullCount))
+        print(problematicIngr)
+
+    # Empty diagonal
     np.fill_diagonal(asims, 0)
 
     return asims
